ff17d49afa5f2b945a6fbd2256cb0a2e52791f7d
Deepak Pokkalla <deepakpokkalla@gmail.com>
2025-05-06 15:37:54 -0400

    multi-head self-attention model

--- GPT-v1/train_gpt_multihead.py
+++ GPT-v1/train_gpt_multihead.py
@@ -3,14 +3,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
-block_size = 8 # what is the maximum context length for predictions? T here!
+batch_size = 64 #32 # how many independent sequences will we process in parallel?
+block_size = 256 #8 # what is the maximum context length for predictions? T here!
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-3
+learning_rate = 3e-4 #1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32 # embedding dimension
+n_embd = 384 #32 # embedding dimension
+n_head = 6 #4
+n_layer = 6 #4
+dropout = 0.2
 # ------------
 
 torch.manual_seed(1337)
@@ -68,8 +71,10 @@ class Head(nn.Module):
         self.key = nn.Linear(n_embd, head_size, bias=False) # (n_embd, hs)
         self.query = nn.Linear(n_embd, head_size, bias=False) # (n_embd, hs)
         self.value = nn.Linear(n_embd, head_size, bias=False) # (n_embd, hs)
+        # non-trainable attribute, available as self.tril
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
-    
+        self.dropout = nn.Dropout(dropout)
+
     def forward(self, x):
         # input of size (batch, time-step, channels or n_embd)
         # output of size (batch, time-step, head size)
@@ -82,29 +87,31 @@ class Head(nn.Module):
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
         # wei = wei.masked_fill(self.tril == 0, float('-inf')) # (B, T, T)
         wei = wei.softmax(dim=-1) # (B,T,T)
+        wei = self.dropout(wei) # (B,T,T)
         # perform the weighted aggregation of the values
         v = self.value(x) # (B,T,hs)
         out = wei @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs); Here, it's (B, T, C) as hs = (n_embd or C) in single-head attention
         return out
 
 class MultiHeadAttention(nn.Module):
-    """ multiple heads of self-attention in parallel"""
+    """ multiple heads of self-attention in parallel """
 
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         # where is this proj in transformers paper? (shown in Fig.2, but in Fig.1 shown outside the block, why?)
-        self.proj = nn.Linear(n_embd, n_embd) 
+        self.proj = nn.Linear(n_embd, n_embd) # n_embd = head_size * num_heads
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         # each communication channel in multi-head attention is smaller compared to single-head attention
         out = torch.cat([h(x) for h in self.heads], dim=-1) # (B, T, num_heads*(n_embd/head_size)) --> (B, T, n_embd)
-        out = self.proj(out)
+        out = self.dropout(self.proj(out)) # (B, T, n_embd) 
         return out
 
 # Self-attention does the communication. FeedForward is per-token level, thinking on the data individually. 
 class FeedForward(nn.Module):
-    """ a simple linear layer followed by a non-linearity"""
+    """ a simple linear layer followed by a non-linearity """
 
     def __init__(self, n_embd):
         super().__init__()
@@ -112,43 +119,52 @@ class FeedForward(nn.Module):
             nn.Linear(n_embd, 4*n_embd), # d_ff (ff inner dimension) is 4*n_embd as per transformers paper (Sec 3.3)
             nn.ReLU(),
             nn.Linear(4*n_embd, n_embd), # 4* adds some computation & grows the layer. 
+            nn.Dropout(dropout),
         )
-    
+
     def forward(self, x):
-        return self.net(x)
+        return self.net(x) # (B, T, n_embd)
 
 class Block(nn.Module):
-    """ Transformer block: communication followed by computation"""
+    """ Transformer block: communication followed by computation """
 
     def __init__(self, n_embd, n_head):
+        # n_embd: embedding dimension, n_head: the number of heads we'd like
         super().__init__()
         head_size = n_embd // n_head
         self.sa = MultiHeadAttention(n_head, head_size) # communication with n_head's & head_size; # i.e., 4 heads of 8-dimensional self-attention
         self.ffwd = FeedForward(n_embd) # computation (B, T, n_embd)
         self.ln1 = nn.LayerNorm(n_embd) # layernorm along n_embd dimension, per-token transformation
         self.ln2 = nn.LayerNorm(n_embd) # layernorm along n_embd dimension, per-token transformation
-    
+
     def forward(self, x):
-        x = x + self.sa(self.ln1(x))
-        x = x + self.ffwd(self.ln2(x))
+        x = x + self.sa(self.ln1(x)) # (B, T, n_embd)
+        x = x + self.ffwd(self.ln2(x)) # (B, T, n_embd)
         return x
 
 # single self-attention head model 
-class BigramLanguageModel(nn.Module):
+class GPTLanguageModel(nn.Module):
 
     def __init__(self):
         super().__init__()
         # nn.Embedding.weight is the learnable weight matrix (num_embeddings/vocab_size, emb_dim)
 
-        self.token_embedding_table = nn.Embedding(vocab_size,n_embd) # token identity embedding, where "n_embd != vocab_size" unlike bigram model
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd) # token identity embedding, where "n_embd != vocab_size" unlike bigram model
         self.position_embedding_table = nn.Embedding(block_size, n_embd) # token position embedding; (T, n_embd)
-        self.blocks = nn.Sequential(
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-            nn.LayerNorm(n_embd),
-        )
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
         self.lm_head = nn.Linear(n_embd, vocab_size) # language modeling head; (n_embd, vocab_size)
+
+        # better init, not covered in the original GPT video, but important, will cover in followup video
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
     def forward(self, idx, targets=None):
         B, T = idx.shape # idx = xb; shape: (B, T)
@@ -160,7 +176,8 @@ class BigramLanguageModel(nn.Module):
         x = tok_emb + pos_emb # broadcasting (B, T, n_embd) + (T, n_embd) --> (B, T, n_embd), "n_embd = C" here!
         # feed it ("x" or encoded inputs) to self-attention head(s): that will apply softmax (ignore future tokens)
         # n_embd is passed to "sa_head" and extracted as C (channel dim) in forward of Head. Thus, "C = n_embd".
-        x = self.blocks(x) # (B, T, n_embd ), "n_embd = C" here!
+        x = self.blocks(x) # (B, T, n_embd), "n_embd = C" here!
+        x = self.ln_f(x) # (B, T, n_embd)
         logits = self.lm_head(x) # (B, T, n_embd) --> (B,T,vocab_size)
         
         # C here is vocab_size 
@@ -195,7 +212,7 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
 
-model = BigramLanguageModel()
+model = GPTLanguageModel()
 model = model.to(device)
 # print the number of parameters in the model
 print(sum(p.numel() for p in model.parameters())/1e6, 'M parameters')
@@ -221,5 +238,5 @@ for iter in range(max_iters):
 
 # generate from the model
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
-# print(decode(model.generate(context, max_new_tokens=500)[0].tolist()))
+print(decode(model.generate(context, max_new_tokens=500)[0].tolist()))
 open('more.txt', 'w').write(decode(model.generate(context, max_new_tokens=10000)[0].tolist()))
